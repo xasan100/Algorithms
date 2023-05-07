6d3f6af996aa64b7a8792a6ed358ada1b1205b70
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-07 14:33:18 +0500

    toq juf

--- number.js
+++ number.js
@@ -1,5 +1,8 @@
-console.log('hello world');
-console.log('hello world');
-console.log('hello world');
 
+
+// function findOddNumbers(numbers) {
+//     a = numbers.filter(number => number % 2 == 0);
+//     b = numbers.filter((number) => number % 2 !== 0)
+//     return `juft ${a} toq ${b} `
+// }
 
